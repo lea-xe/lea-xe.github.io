6ba70f797262c8dced235e049d1fdeb250886bb8
Lea Etzlinger <letzlinger.mmt-b2022@fh-salzburg.ac.at>
2024-03-02 18:15:01 +0100

    added pages

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./src/**/**/*.*",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#5B5853',
+        'beige': '#D5CEBE',
+        'gray': '#817C75',
+        'light': '#F5F4EF',
+      },
+      fontFamily: {
+        'primary': ['"EB Garamond"', 'system-ui', 'sans-serif'],
+        'secondary': ['Robika', 'serif'],
+      },
+      spacing: {
+        '128': '32rem',
+        '200': '40rem'
+      },
+    },
+  },
+  plugins: [],
+}
